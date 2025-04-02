9a58830618807b4596ea6d51640b18cb3c9611d0
LuckRookie <13007600936@163.com>
2025-04-02 10:53:57 +0800

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sciretriever!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/Retriver/CJEM.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+class CJEM:
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    cjem = CJEM()
+    cjem.get_cjem()
