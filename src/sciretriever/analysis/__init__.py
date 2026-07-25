0ad38fce6cefb19f9e47865ddede997d6950b025
LuckRookie <13007600936@163.com>
2026-07-25 12:41:49 +0800

    refactor: remove parallel backfill services
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/sciretriever/analysis/__init__.py
+++ src/sciretriever/analysis/__init__.py
@@ -3,10 +3,8 @@
 from .contracts import AnalysisDocument, AnalysisProviderRequest, AnalysisProviderResponse, AnalysisReference, AnalysisSection, CanonicalFieldProposal, GeneratedTag, NewEntityProposal, NewTagProposal, SECTION_IDS
 from .provider import AnalysisProvider, OpenAICompatibleAnalysisProvider
 from .service import ANALYSIS_KIND, ANALYSIS_MEDIA_TYPE, ANALYSIS_SCHEMA_VERSION, AnalysisResult, AnalysisService, analysis_response_schema, parse_analysis_response
-from .backfill import AnalysisBackfillResult, AnalysisBackfillService, WorkVersionAnalysisOutcome
 
 __all__ = ("ANALYSIS_KIND", "ANALYSIS_MEDIA_TYPE", "ANALYSIS_SCHEMA_VERSION", "AnalysisDocument", "AnalysisProvider",
            "AnalysisProviderRequest", "AnalysisProviderResponse", "AnalysisReference", "AnalysisResult", "AnalysisSection",
            "AnalysisService", "CanonicalFieldProposal", "GeneratedTag", "NewEntityProposal", "NewTagProposal",
-           "OpenAICompatibleAnalysisProvider", "SECTION_IDS", "analysis_response_schema", "parse_analysis_response",
-           "AnalysisBackfillResult", "AnalysisBackfillService", "WorkVersionAnalysisOutcome")
+           "OpenAICompatibleAnalysisProvider", "SECTION_IDS", "analysis_response_schema", "parse_analysis_response")
